2d36a71b099946e965304371f0774e20861587ce
Younesse Kaddar <youqad@gmail.com>
2026-02-17 07:37:06 +0000

    refactor: delete distributed/, training/, trainers/ modules

--- src/synthstats/training/buffers/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Training buffers for trajectory storage."""
-
-from synthstats.train.loop.replay import BufferEntry, GFNReplayBuffer, ReplayBuffer
-
-__all__ = ["BufferEntry", "GFNReplayBuffer", "ReplayBuffer"]
