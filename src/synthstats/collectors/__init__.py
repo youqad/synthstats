decbea43f43b2b7fca0dd0a7122138820e634ca3
Younesse Kaddar <youqad@gmail.com>
2026-02-18 11:52:19 +0000

    chore: drop skyrl_ray, subtb_torch, stale stubs

--- src/synthstats/collectors/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Collectors package.
-
-Canonical implementations live in train/loop/:
-- TrajectoryCollector, CollectedTrajectory -> train.loop.collectors
-- build_subtb_batch, build_tinker_batch -> train.loop.batching
-"""
